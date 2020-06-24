a77c646de424810115511cfa4dcd5811d2c92a37
Gerard C. Saperas <gcsaperas@gmail.com>
2020-06-24 12:15:18 +0200

    Added user creation logic and password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { body, validationResult } = require('express-validator');
+
+// Require user's model
 const User = require('../models/User');
 
 // @route   POST api/users
@@ -16,10 +20,40 @@ router.post(
 		// password must be at least 6 chars long
 		body('password', 'Password must be 6 or more characters long').isLength({ min: 6 })
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { username, email, password } = req.body;
+
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+
+			if (user) {
+				return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+			}
+			// Get user's gravatar
+			const avatar = gravatar.url(email, {
+				size: '200',
+				rating: 'pg',
+				default: 'monsterid'
+			});
+			// Create a new instance of User
+			user = new User({ username, email, password, avatar });
+			// Encrypt password using bcrypt
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+			//Save the user
+			await user.save();
+			// Return jsonwebtoken
+			res.send('User registered');
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server error');
 		}
 
 		res.send('User route');
