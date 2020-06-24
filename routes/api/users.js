1f66ca00e455605cebe0f89e6e6cfeaf5c48197a
Gerard C. Saperas <gcsaperas@gmail.com>
2020-06-24 11:35:44 +0200

    Added POST api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => {
-	res.send('User route');
-});
+router.post(
+	'/',
+	[
+		// username must be filled
+		body('username', 'Username is required').notEmpty(),
+		// email is valid and filled
+		body('email', 'Valid email is required').isEmail(),
+		// password must be at least 6 chars long
+		body('password', 'Password must be 6 or more characters long').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('User route');
+	}
+);
 
 module.exports = router;
